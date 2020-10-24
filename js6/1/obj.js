6eafc576f35f4d4a6504e330dfa3583be8bfaad3
Kasahara-Yumi <Kasahara-Yumi@post.appleworld.com>
2020-10-24 12:57:31 +0900

    チュートリアル

---
+++ js6/1/obj.js
@@ -0,0 +1,13 @@
+const obj = {
+    "key" : "value"
+}
+
+console.log(obj.key);
+console.log(obj["key"]);
+
+const object = {
+    "123" : "abc"
+}
+
+//console.log(object.123);
+console.log(object["123"]);
